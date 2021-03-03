f3ece96c724d2db216a106025bd8663a3e41b268
MR.Rezoo <rezam578@gmail.com>
2021-03-03 16:55:48 +0330

    Initial commit 3

--- fixer/handler.py
+++ fixer/handler.py
@@ -1,2 +1,12 @@
-def get_rate():
-    print("get rate ")
+import json
+
+import requests
+
+BASE_PATH = 'http://data.fixer.io/api/latest?access_key='
+
+
+def get_rates(api_key):
+    response = requests.get(BASE_PATH + api_key)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return None
--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name="fixer-demo",
-    version="0.2",
+    version="0.3",
     description="Fixer service demo packages",
     url="https://github.com/MrRezoo/fixer-demo.git",  # Git repository
     author="Reza Mobaraki",
     author_email="rezam578@gmail.com",
     license="MIT",
     pakages=['fixer'],
+    install_requires=['requests'],
     zip_safe=False
 )

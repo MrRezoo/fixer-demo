fb0f67686085582be9541d18feb42cc49e1a23ce
MR.Rezoo <rezam578@gmail.com>
2021-03-03 14:40:05 +0330

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="fixer-demo",
+    version="0.1",
+    description="Fixer service demo packages",
+    url="https://github.com/MrRezoo/fixer-demo.git",  # Git repository
+    author="Reza Mobaraki",
+    author_email="rezam578@gmail.com",
+    license="MIT",
+    pakages=['fixer'],
+    zip_safe=False
+)
